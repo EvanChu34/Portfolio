b66632dd171bef6730b7b03cdca8e17b0b097229
Evan Chu <echu34@gmail.com>
2021-01-10 22:30:29 -0500

    updated code

---
+++ script.js
@@ -0,0 +1,36 @@
+var qcPageBtn = document.getElementById("#qcPage");
+var qcGitBtn = document.getElementById("#qcGit");
+var weatherPageBtn = document.getElementById("#weatherPage");
+var weatherGitBtn = document.getElementById("#weatherGit");
+var passPageBtn = document.getElementById("passPage#");
+var passGitBtn = document.getElementById("passGit#");
+
+qcPageBtn.addEventListener("click", function(){
+
+});
+
+qcGitBtn.addEventListener("click", function(){
+    
+});
+
+
+weatherPageBtn.addEventListener("click", function(){
+    
+});
+
+
+weatherGitBtn.addEventListener("click", function(){
+    
+});
+
+
+passPageBtn.addEventListener("click", function(){
+    
+});
+
+
+passGitBtn.addEventListener("click", function(){
+    
+});
+
+
