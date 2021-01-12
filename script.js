5cbd58eb1e41c5033876c97620e082e2df02df70
Evan Chu <echu34@gmail.com>
2021-01-11 19:30:23 -0500

    Added script buttons

--- script.js
+++ script.js
@@ -1,36 +1,35 @@
-var qcPageBtn = document.getElementById("#qcPage");
-var qcGitBtn = document.getElementById("#qcGit");
-var weatherPageBtn = document.getElementById("#weatherPage");
-var weatherGitBtn = document.getElementById("#weatherGit");
-var passPageBtn = document.getElementById("passPage#");
-var passGitBtn = document.getElementById("passGit#");
+const qcPageBtn = document.getElementById("qcPage");
+const qcGitBtn = document.getElementById("qcGit");
+const weatherPageBtn = document.getElementById("weatherPage");
+const weatherGitBtn = document.getElementById("weatherGit");
+const passPageBtn = document.getElementById("passPage");
+const passGitBtn = document.getElementById("passGit");
 
 qcPageBtn.addEventListener("click", function(){
-
+    location.href = "https://ctobin25.github.io/quarantine-and-chill/";
 });
-
+ 
 qcGitBtn.addEventListener("click", function(){
-    
+   location.href = "https://github.com/ctobin25/quarantine-and-chill.git";
 });
-
 
 weatherPageBtn.addEventListener("click", function(){
-    
+    location.href = "https://evanchu34.github.io/WeatherDash/";
 });
 
 
 weatherGitBtn.addEventListener("click", function(){
-    
+    location.href = "https://github.com/EvanChu34/WeatherDash.git";
 });
 
 
 passPageBtn.addEventListener("click", function(){
-    
+    location.href = "https://evanchu34.github.io/RandomPasswordGenerator/";
 });
 
 
 passGitBtn.addEventListener("click", function(){
-    
+    location.href = "https://github.com/EvanChu34/RandomPasswordGenerator.git";
 });
 
 
