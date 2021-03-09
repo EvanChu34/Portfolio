d76d76f06cbb8c6d1270aee441d2259c538daeac
Evan Chu <echu34@gmail.com>
2021-03-08 21:33:38 -0500

    adding buttons and functions

--- script.js
+++ script.js
@@ -4,6 +4,12 @@ const weatherPageBtn = document.getElementById("weatherPage");
 const weatherGitBtn = document.getElementById("weatherGit");
 const passPageBtn = document.getElementById("passPage");
 const passGitBtn = document.getElementById("passGit");
+const dadPageBtn = document.getElementById("dadPage");
+const dadGitBtn = document.getElementById("dadGit");
+const trackerPageBtn = document.getElementById("trackerPage");
+const trackerGitBtn = document.getElementById("trackerGit");
+const burgerPageBtn = document.getElementById("burgerPage");
+const burgerGitBtn = document.getElementById("burgerGit");
 
 qcPageBtn.addEventListener("click", function(){
     location.href = "https://ctobin25.github.io/quarantine-and-chill/";
@@ -17,19 +23,39 @@ weatherPageBtn.addEventListener("click", function(){
     location.href = "https://evanchu34.github.io/WeatherDash/";
 });
 
-
 weatherGitBtn.addEventListener("click", function(){
     location.href = "https://github.com/EvanChu34/WeatherDash.git";
 });
-
 
 passPageBtn.addEventListener("click", function(){
     location.href = "https://evanchu34.github.io/RandomPasswordGenerator/";
 });
 
-
 passGitBtn.addEventListener("click", function(){
     location.href = "https://github.com/EvanChu34/RandomPasswordGenerator.git";
 });
 
+dadPageBtn.addEventListener("click", function(){
+    location.href = "https://aqueous-meadow-09339.herokuapp.com/";
+});
+
+passGitBtn.addEventListener("click", function(){
+    location.href = "https://github.com/puentebravo/project2";
+});
+
+trackerPageBtn.addEventListener("click", function(){
+    location.href = "https://github.com/EvanChu34/EmployeeTracker";
+});
+
+trackerGitBtn.addEventListener("click", function(){
+    location.href = "https://evanchu34.github.io/EmployeeTracker/";
+});
+
+burgerPageBtn.addEventListener("click", function(){
+    location.href = "https://fast-fjord-72205.herokuapp.com/";
+});
+
+burgerGitBtn .addEventListener("click", function(){
+    location.href = "https://github.com/EvanChu34/BurgerPlz";
+});
 
